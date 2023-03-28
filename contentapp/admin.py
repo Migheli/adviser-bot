b95760a5b57c2baed0cf544848e1c41eaff87b80
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-28 22:36:38 +0300

    add first models and register it

--- contentapp/admin.py
+++ contentapp/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from .models import Chapter, Article
 # Register your models here.
+
+
+@admin.register(Chapter)
+class RestaurantAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'name'
+    ]
+
+
+@admin.register(Article)
+class RestaurantAdmin(admin.ModelAdmin):
+
+    list_display = [
+        'name',
+        'text',
+        'chapter',
+    ]
+ 
+ 
--- contentapp/models.py
+++ contentapp/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Chapter(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=50
+    )
+
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+    def __str__(self):
+        return self.name
+
+
+class Article(models.Model):
+    name = models.CharField(
+        'Название',
+        max_length=50
+    )
+
+    text = models.TextField(
+        'Текст',
+        max_length=3000
+    )
+
+    
+    chapter = models.ForeignKey(
+	    Chapter,
+		verbose_name='Раздел',
+		related_name='articles',
+		null=True,
+		blank=True,
+		on_delete=models.SET_NULL,
+	)
+
+    class Meta:
+        verbose_name = 'Статья'
+        verbose_name_plural = 'Статьи'
+
+    def __str__(self):
+        return self.name
