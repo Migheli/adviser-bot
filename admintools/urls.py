9649ec85f080a4202a70605f709edeb51c46dc1e
Migheli <98918370+Migheli@users.noreply.github.com>
2023-03-29 23:55:40 +0300

    add primitive api response for main menu

--- admintools/urls.py
+++ admintools/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from contentapp import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('main/', views.get_main_menu_data)
 ]
--- contentapp/views.py
+++ contentapp/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Chapter
+from django.http import JsonResponse
+
 
 # Create your views here.
+def get_main_menu_data(request):
+    chapters = Chapter.objects.all()
+
+    dumped_chapters = []
+    for chapter in chapters:
+        dumped_chapter = {
+            'name': chapter.name,
+        }
+        dumped_chapters.append(dumped_chapter)
+    return JsonResponse(dumped_chapters, safe=False, json_dumps_params={
+        'ensure_ascii': False,
+        'indent': 4,
+    })
